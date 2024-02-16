a12f136271982235d9b4a95582e2e6ff7034bf93
Aminemh <mahdiamine@outlook.com>
2024-02-16 09:30:51 +0100

    ff

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home Page</title>
+  <!-- Materialize CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 center-align">
+        <h1>Welcome to our Website</h1>
+        <div class="row">
+          <div class="col s6">
+            <a href="admin/controllers/controller-signin.php" class="waves-effect waves-light btn-large blue darken-2">Sign in as Enterprise</a>
+          </div>
+          <div class="col s6">
+            <a href="PHPBDD/controllers/controller-signin.php" class="waves-effect waves-light btn-large green darken-2">Sign in as User</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- jQuery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <!-- Materialize JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
